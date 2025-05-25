9c819b1f7ab33e3377c87da94305412f18b0e756
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2025-05-26 00:20:39 +0300

    Normalize line endings

--- src/Services/Spotify/SpotifyAuthService.cs
+++ src/Services/Spotify/SpotifyAuthService.cs
@@ -116,8 +116,6 @@ namespace SpotifyWebAPI_Intro.Services.Spotify
               { "grant_type", grantType },
               { "refresh_token", refreshToken },
               { "client_id", clientID },
-              
-              // client_secret detected
               { "client_secret", clientSecret }
             };
 
