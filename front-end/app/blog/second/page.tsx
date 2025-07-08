a0b46baa9655217b76ec4f2e3ffed8b79b7d2837
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2025-07-08 06:19:17 +0300

    Front-end start fresh

--- front-end/app/blog/second/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function SeconBlog() {
-    return <h1>Second blog post</h1>;
-}
--- front-end/app/products/[productId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProductDetails({
-    params,
-}: {
-    params: { productId: string };
-}) {
-    return <h1> Product Id {params.productId} </h1>;
-}
